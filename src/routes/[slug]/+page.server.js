550cc57d0af885a354d1306686b8454ff6ff5fef
vytenisstaugaitis <30520456+vytenisstaugaitis@users.noreply.github.com>
2025-04-10 12:19:52 +0300

    feature: resize

--- src/routes/[slug]/+page.server.js
+++ src/routes/[slug]/+page.server.js
@@ -5,7 +5,7 @@ import { db } from '$lib/server/db'
 import { formatDate } from '$lib'
 import { redirect } from '@sveltejs/kit'
 import { error } from '@sveltejs/kit'
-
+import ffmpeg from 'fluent-ffmpeg'
 
 export const load = async ({url, params}) => {
     const id = params.slug
@@ -22,6 +22,46 @@ export const load = async ({url, params}) => {
     if (!existsSync(`uploads/${id}`)) {
         return {
             status: 404
+        }
+    }
+
+    if (!selectedFile.width && !selectedFile.height) {
+        const metaData = await new Promise((resolve, reject) => {
+            ffmpeg.ffprobe(`uploads/${id}`, (err, metadata) => {
+                if (err) reject(err)
+                resolve(metadata)
+            })
+        })
+
+        const { streams } = metaData
+        const videoStream = streams.find(stream => stream.codec_type === 'video')
+        if ( videoStream && videoStream.width && videoStream.height) {
+            selectedFile.width = videoStream.width
+            selectedFile.height = videoStream.height
+
+            await db.update(filesTable)
+            .set({
+                width: videoStream.width,
+                height: videoStream.height
+            })
+            .where(eq(filesTable.uuid, id))
+        }
+    }
+
+    // if compressed, get the width and height
+    if (existsSync(`compressed/${id}`)) {
+        const metadata = await new Promise((resolve, reject) => {
+            ffmpeg.ffprobe(`compressed/${id}`, (err, metadata) => {
+                if (err) reject(err)
+                resolve(metadata)
+            })
+        })
+
+        const { streams } = metadata
+        const videoStream = streams.find(stream => stream.codec_type === 'video')
+        if ( videoStream && videoStream.width && videoStream.height) {
+            selectedFile.new_width = videoStream.width
+            selectedFile.new_height = videoStream.height
         }
     }
 
@@ -58,6 +98,7 @@ export const actions = {
         const id = params.slug
         const quality = data.get('quality')
         const noaudio = data.get('noaudio')
+        const width = data.get('width') || 0
 
         await db.update(filesTable).set({compressing: 1}).where(eq(filesTable.uuid, id))
         await db.update(filesTable).set({start_date: formatDate(new Date())}).where(eq(filesTable.uuid, id))
@@ -69,7 +110,8 @@ export const actions = {
             },
             body: JSON.stringify({
                 quality,
-                noaudio
+                noaudio,
+                width,
             })
         })
 
@@ -90,7 +132,7 @@ export const actions = {
 
                 await db.update(filesTable).set({compressing: 0}).where(eq(filesTable.uuid, id))
                 await db.update(filesTable).set({processed: 1}).where(eq(filesTable.uuid, id))
-                await db.update(filesTable).set({end_date: formatDate(new Date())}).where(eq(filesTable.uuid, id))
+                await db.update(filesTable).set({end_date: formatDate(new Date(), 0, true)}).where(eq(filesTable.uuid, id))
                 await db.update(filesTable).set({quality}).where(eq(filesTable.uuid, id))
                 await db.update(filesTable).set({expiry_date: formatDate(new Date(), 1)}).where(eq(filesTable.uuid, id))
                 await db.update(filesTable).set({size: size.toFixed(2)}).where(eq(filesTable.uuid, id))
--- src/routes/api/download/[slug]/+server.js
+++ src/routes/api/download/[slug]/+server.js
@@ -7,7 +7,9 @@ import { db } from '$lib/server/db'
 export async function GET({ url, params }) {
     const id = params.slug
     let path = `compressed/${id}`
-    let poster = false
+    let poster = url.searchParams.get('poster')
+    let width = url.searchParams.get('w')
+    let quality = url.searchParams.get('q')
 
     let selectedFile = await db.select().from(filesTable).where(eq(filesTable.uuid, id))
 
@@ -27,8 +29,11 @@ export async function GET({ url, params }) {
         name = `${selectedFile.name}.mp4`
     }
 
-    if (url.searchParams.get('poster')) {
-        poster = true
+    if (width && quality) {
+        name = name.replace('.mp4', `-${url.searchParams.get('w')}w-q${quality}.mp4`)
+    }
+
+    if (poster === 'true') {
         path = `compressed/posters/${id}`
         name = `${selectedFile.name}.jpg`
     }
